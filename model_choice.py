428dde61b29ce8d66c6b2cb6a281c7f82a7b061e
Félix <felixmolina8@gmail.com>
2018-02-05 20:31:01 +0100

    code test

--- model_choice.py
+++ model_choice.py
@@ -7,29 +7,29 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import SGD
 
-# Generate dummy data
 import numpy as np
-x_train = np.random.random((1000, 20))
-y_train = keras.utils.to_categorical(np.random.randint(10, size=(1000, 1)), num_classes=10)
-x_test = np.random.random((100, 20))
-y_test = keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
+
+x_train = np.random.random((1000, 13)) #nb de dataset , iteration de ces datasets
+y_train = keras.utils.to_categorical(np.random.randint(2, size=(1000, 1)), num_classes=2)
+x_test = np.random.random((100, 13))
+y_test = keras.utils.to_categorical(np.random.randint(2, size=(100, 1)), num_classes=2)
 
 model = Sequential()
-# Dense(64) is a fully-connected layer with 64 hidden units.
-# in the first layer, you must specify the expected input data shape:
-# here, 20-dimensional vectors.
-model.add(Dense(64, activation='relu', input_dim=20))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(10, activation='softmax'))
 
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy',
-              optimizer=sgd,
-              metrics=['accuracy'])
+model.add(Dense(26, activation='softmax', input_dim=13))
+model.add(Dropout(0.5))
+model.add(Dense(13, activation='softmax'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax'))
 
-model.fit(x_train, y_train,
-          epochs=20,
-          batch_size=128)
-score = model.evaluate(x_test, y_test, batch_size=128)
+keras.utils.plot_model(model, show_shapes=True, rankdir="LR")
+
+sgd = SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='mean_squared_error', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=100, batch_size=20)
+score = model.evaluate(x_test, y_test, batch_size=20)
+
+keras.utils.plot_model(model, show_shapes=True)
