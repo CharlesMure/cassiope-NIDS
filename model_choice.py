aba84b94380c49d3a3235a8f7b839983974ec108
Charles MURE <charles.mure@telecom-sudparis.eu>
2018-02-14 15:49:24 +0100

    Finalisation preprocessing NSL-KDD + Modification modèle

--- model_choice.py
+++ model_choice.py
@@ -3,70 +3,115 @@
 # -*- coding: utf8 -*-
 
 import keras
+import csv
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
 from keras.optimizers import SGD
 
-import numpy
+import numpy as np
 
-import scipy
-import scipy.io.arff
 
 from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
-def arff_to_ndarray(path_to_arff_file):
-    """
-    Converts content of .arff file to numpy matrix.
-    :param path_to_arff_file:
-    :return: numpy.ndarray matrix for feature values, vector with labels/classes.
-    """
-
-    # Load as numpy objects.
-    data, meta = scipy.io.arff.loadarff(path_to_arff_file)
-
-    # Extract labels.
-    labels = data[meta.names()[-1]]
-
-    # Discard last column (labels).
-    data = data[meta.names()[:-1]]
-
-    # Use view(numpy.float) to convert elements from numpy.void to numpy.float. Use -1 to let numpy infer the shape.
-    data = data.view().reshape(data.shape)
-
-    return data, labels
+def transform(x):
+    return {
+        'back': 'back',
+        'buffer_overflow': 'buffer_overflow',
+        'ftp_write': 'ftp_write',
+        'guess_passwd': 'guess_passwd',
+        'imap': 'imap',
+        'ipsweep': 'ipsweep',
+        'land': 'land',
+        'loadmodule': 'loadmodule',
+        'multihop': 'multihop',
+        'neptune': 'neptune',
+        'nmap': 'nmap',
+        'perl': 'perl',
+        'phf': 'phf',
+        'pod': 'pod',
+        'portsweep': 'portsweep',
+        'rootkit': 'rootkit',
+        'satan': 'satan',
+        'smurf': 'smurf',
+        'spy': 'spy',
+        'teardrop': 'teardrop',
+        'warezclient': 'warezclient',
+        'warezmaster': 'warezmaster',
+        'normal': 'normal',
+    }.get(x, 'unknown') 
 
 def generate_model(shape):
     # define the model
     model = keras.models.Sequential()
 
     model.add(Dense(82, input_dim=shape, kernel_initializer='uniform', activation='relu'))
-    model.add(Dropout(0.4))
-    model.add(Dense(41, activation='relu'))
-    model.add(Dropout(0.4))
-    model.add(Dense(20, activation='softmax'))
-    model.add(Dropout(0.4))
-    model.add(Dense(10, activation='softmax'))
-    model.add(Dropout(0.4))
-    model.add(Dense(2, activation='softmax'))
+    model.add(Dropout(0.1))
+    model.add(Dense(82, activation='relu'))
+    model.add(Dropout(0.1))
+    model.add(Dense(41, activation='softmax'))
+    model.add(Dropout(0.2))
+    model.add(Dense(24, activation='softmax'))
     print(model.summary())
 
     return model
 
 encoder = LabelBinarizer()
+encoder2 = LabelEncoder()
 
-x_train, train_label = arff_to_ndarray("KDDTrain+_20Percent.arff")
-x_train = numpy.array(x_train)
-y_train = encoder.fit_transform(train_label)
-y_train = numpy.array(y_train)
+filereader = csv.reader(open("Data/KDDTrain+.txt"), delimiter=",")
+data = np.array(list(filereader))
 
-x_test, test_label = arff_to_ndarray("KDDTest+.arff")
-x_test = numpy.array(x_test)
-y_test = encoder.fit_transform(test_label)
-y_test = numpy.array(y_test)
+x_train = data[:,np.arange(0,41)]
+x_train[:,1] = encoder2.fit_transform(x_train[:,1])
+x_train[:,2] = encoder2.fit_transform(x_train[:,2])
+x_train[:,3] = encoder2.fit_transform(x_train[:,3])
 
+train_label = data[:,41]
+#y_train = encoder.fit_transform(train_label)
+train_label = [transform(attacktype) for attacktype in train_label]
+
+filereaderTest = csv.reader(open("Data/KDDTest+.txt"), delimiter=",")
+dataTest = np.array(list(filereaderTest))
+
+
+
+x_test = dataTest[:,np.arange(0,41)]
+x_test[:,1] = encoder2.fit_transform(x_test[:,1])
+x_test[:,2] = encoder2.fit_transform(x_test[:,2])
+x_test[:,3] = encoder2.fit_transform(x_test[:,3])
+
+test_label = dataTest[:,41]
+test_label = [transform(attacktype) for attacktype in test_label]
+
+label = np.concatenate((train_label,test_label))
+#print(np.shape(label))
+
+y = encoder.fit_transform(label)
+#print(label)
+trainlen = np.size(train_label)
+testlen = np.size(test_label)
+y_train = y[np.arange(0,trainlen)]
+y_test = y[np.arange(trainlen,trainlen+testlen)]
+
+## Generation d'un dataset de validation
+seed = 7
+np.random.seed(seed)
+x_validation, x_test, y_validation, y_test = train_test_split(x_test, y_test, test_size=0.75, random_state=seed)
+
+print(np.shape(y_train))
+print(np.shape(y_test))
+print(np.shape(x_train))
+print(np.shape(x_test))
+ 
 model = generate_model(41)
 
 model.compile(loss=keras.losses.mean_squared_error,
-              optimizer=keras.optimizers.SGD(lr=0.01, momentum=0.9, nesterov=True))
+              optimizer=keras.optimizers.SGD(lr=0.005, momentum=0.9, nesterov=True),metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=10)
+model.fit(x_train, y_train,validation_data=(x_validation,y_validation), epochs=30, batch_size=15)
+
+score = model.evaluate(x_test, y_test, verbose=0)
+
+print("precision on test data:", score[1]*100, "%")
