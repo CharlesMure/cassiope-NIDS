7208bc5b76573b2e5cf9827f8e401ad2e3bbaa11
Charles Mure <charles.mure@telecom-sudparis.eu>
2018-03-06 20:42:20 +0100

    Ajout GridSearchCV

--- model_choice.py
+++ model_choice.py
@@ -13,14 +13,14 @@ from keras.models import Sequential
 from keras.layers import Conv2D, GlobalAveragePooling1D, MaxPooling2D
 from keras.layers import Dense, Dropout, Activation, Embedding, BatchNormalization, Flatten
 from keras.optimizers import SGD
+from keras.wrappers.scikit_learn import KerasClassifier
 
-import h5py
 import numpy as np
 
 
-from sklearn.preprocessing import LabelBinarizer, MinMaxScaler
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer, MinMaxScaler, LabelEncoder
+from sklearn.model_selection import train_test_split, GridSearchCV
+
 
 def transform(x):
     return {
@@ -63,7 +63,8 @@ def transform(x):
         'sqlattack': 'u2r',
         'xterm': 'u2r',
         'normal': 'normal'
-    }.get(x, 'unknown') 
+    }.get(x, 'unknown')
+
 
 def generate_model(shape):
     # define the model
@@ -90,16 +91,16 @@ def generate_cnn_model(shape):
         https://www.researchgate.net/publication/319717354_A_Few-shot_Deep_Learning_Approach_for_Improved_Intrusion_Detection
     '''
     model = Sequential()
-    model.add(Conv2D(64,(3,1),activation='relu',input_shape=(shape,1,1)))
-    model.add(Conv2D(64,(3,1),activation='relu'))
+    model.add(Conv2D(64, (3, 1), activation='relu', input_shape=(shape, 1, 1)))
+    model.add(Conv2D(64, (3, 1), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 1)))
-    model.add(Conv2D(128,(3,1),activation='relu'))
-    model.add(Conv2D(128,(3,1),activation='relu',padding="same"))
-    model.add(Conv2D(128,(3,1),activation='relu',padding="same"))
+    model.add(Conv2D(128, (3, 1), activation='relu'))
+    model.add(Conv2D(128, (3, 1), activation='relu', padding="same"))
+    model.add(Conv2D(128, (3, 1), activation='relu', padding="same"))
     model.add(MaxPooling2D(pool_size=(2, 1)))
-    model.add(Conv2D(256,(3,1),activation='relu',padding="same"))
-    model.add(Conv2D(256,(3,1),activation='relu',padding="same"))
-    model.add(Conv2D(256,(3,1),activation='relu',padding="same"))
+    model.add(Conv2D(256, (3, 1), activation='relu', padding="same"))
+    model.add(Conv2D(256, (3, 1), activation='relu', padding="same"))
+    model.add(Conv2D(256, (3, 1), activation='relu', padding="same"))
     model.add(MaxPooling2D(pool_size=(2, 1)))
     model.add(Flatten())
     model.add(Dense(256, activation='relu'))
@@ -110,44 +111,64 @@ def generate_cnn_model(shape):
     return model
 
 
+def create_model(optimizer='rmsprop', init='glorot_uniform'):
+
+    model = keras.models.Sequential()
+
+    model.add(Dense(82, input_dim=41, activation='relu'))
+    model.add(Dropout(0.1))
+    model.add(Dense(164, kernel_initializer=init, activation='relu'))
+    model.add(Dense(164, kernel_initializer=init, activation='relu'))
+    model.add(Dense(82, kernel_initializer=init, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(41, kernel_initializer=init, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(20, kernel_initializer=init, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(6, kernel_initializer=init, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=optimizer, metrics=['accuracy'])
+    return model
+
+
 def preporcess(data, dataTest, CNN=False):
     scaler = MinMaxScaler()
     encoder = LabelBinarizer()
     encoder2 = LabelEncoder()
 
-    x_train = data[:, np.arange(0,41)]
-    x_train[:,1] = encoder2.fit_transform(x_train[:,1])
-    x_train[:,2] = encoder2.fit_transform(x_train[:,2])
-    x_train[:,3] = encoder2.fit_transform(x_train[:,3])
+    x_train = data[:, np.arange(0, 41)]
+    x_train[:, 1] = encoder2.fit_transform(x_train[:, 1])
+    x_train[:, 2] = encoder2.fit_transform(x_train[:, 2])
+    x_train[:, 3] = encoder2.fit_transform(x_train[:, 3])
 
     scaler.fit(x_train)
     x_train = scaler.transform(x_train)
 
-    train_label = data[:,41]
-    #y_train = encoder.fit_transform(train_label)
+    train_label = data[:, 41]
+    # y_train = encoder.fit_transform(train_label)
     train_label = [transform(attacktype) for attacktype in train_label]
 
-    x_test = dataTest[:,np.arange(0,41)]
-    x_test[:,1] = encoder2.fit_transform(x_test[:,1])
-    x_test[:,2] = encoder2.fit_transform(x_test[:,2])
-    x_test[:,3] = encoder2.fit_transform(x_test[:,3])
+    x_test = dataTest[:, np.arange(0, 41)]
+    x_test[:, 1] = encoder2.fit_transform(x_test[:, 1])
+    x_test[:, 2] = encoder2.fit_transform(x_test[:, 2])
+    x_test[:, 3] = encoder2.fit_transform(x_test[:, 3])
 
     scaler.fit(x_test)
     x_test = scaler.transform(x_test)
 
-    test_label = dataTest[:,41]
+    test_label = dataTest[:, 41]
     test_label = [transform(attacktype) for attacktype in test_label]
 
     # Join dataset => meme encoding pour train et test
-    label = np.concatenate((train_label,test_label))
+    label = np.concatenate((train_label, test_label))
     y = encoder.fit_transform(label)
 
     trainlen = np.size(train_label)
     testlen = np.size(test_label)
-    y_train = y[np.arange(0,trainlen)]
-    y_test = y[np.arange(trainlen,trainlen+testlen)]
+    y_train = y[np.arange(0, trainlen)]
+    y_test = y[np.arange(trainlen, trainlen+testlen)]
 
-    
     if(CNN):
         x_final_train = []
         x_final_test = []
@@ -162,81 +183,25 @@ def preporcess(data, dataTest, CNN=False):
         x_test = np.array(x_final_test)
 
         print(np.shape(x_test))
-    
-    ## Generation d'un dataset de validation
+
+    # Generation d'un dataset de validation
     seed = 9
     np.random.seed(seed)
-    x_validation, x_test, y_validation, y_test = train_test_split(x_test, y_test, test_size=0.70, random_state=seed)
+    x_validation, x_test, y_validation, y_test = train_test_split(
+        x_test, y_test, test_size=0.70, random_state=seed)
 
     return x_train, y_train, x_validation, y_validation, x_test, y_test
 
 
-def CapsNet(input_shape, n_class, routings):
-    """
-    A Capsule Network on MNIST.
-    :param input_shape: data shape, 3d, [width, height, channels]
-    :param n_class: number of classes
-    :param routings: number of routing iterations
-    :return: Two Keras Models, the first one used for training, and the second one for evaluation.
-            `eval_model` can also be used for training.
-    """
-    x = layers.Input(shape=input_shape)
-
-    # Layer 1: Just a conventional Conv2D layer
-    conv1 = layers.Conv2D(filters=64, kernel_size=(3,1), strides=1, padding='valid', activation='relu', name='conv1')(x)
-
-    # Layer 2: Conv2D layer with `squash` activation, then reshape to [None, num_capsule, dim_capsule]
-    primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=1, kernel_size=(3,1), strides=2, padding='valid')
-
-    # Layer 3: Capsule layer. Routing algorithm works here.
-    digitcaps = CapsuleLayer(num_capsule=n_class, dim_capsule=16, routings=routings,
-                             name='digitcaps')(primarycaps)
-
-    # Layer 4: This is an auxiliary layer to replace each capsule with its length. Just to match the true label's shape.
-    # If using tensorflow, this will not be necessary. :)
-    out_caps = Length(name='capsnet')(digitcaps)
-
-    # Decoder network.
-    y = layers.Input(shape=(n_class, 1))
-    masked_by_y = Mask()([digitcaps + 1, y])  # The true label is used to mask the output of capsule layer. For training
-
-    # Shared Decoder model in training and prediction
-    decoder = models.Sequential(name='decoder')
-    decoder.add(layers.Dense(512, activation='relu', input_dim=16 * n_class))
-    decoder.add(layers.Dense(1024, activation='relu'))
-    decoder.add(layers.Dense(np.prod(input_shape), activation='sigmoid'))
-    decoder.add(layers.Reshape(target_shape=input_shape, name='out_recon'))
-
-    # Models for training and evaluation (prediction)
-    train_model = models.Model([x, y], [out_caps, decoder(masked_by_y)])
-
-    return train_model
-
-
-def margin_loss(y_true, y_pred):
-    """
-    Margin loss for Eq.(4). When y_true[i, :] contains not just one `1`, this loss should work too. Not test it.
-    :param y_true: [None, n_classes]
-    :param y_pred: [None, num_capsule]
-    :return: a scalar loss value.
-    """
-    L = y_true * K.square(K.maximum(0., 0.9 - y_pred)) + \
-        0.5 * (1 - y_true) * K.square(K.maximum(0., y_pred - 0.1))
-
-    return K.mean(K.sum(L, 1))
-
-
-def eval(model,x_test,y_test):
+def eval(model, x_test, y_test):
     score = model.evaluate(x_test, y_test, verbose=1)
     print("loss on test data:", score[0])
     print("accuracy on test data:", score[1]*100, "%")
 
 
-
-
 def main():
 
-    input_list = ["1. MLP", "2. CNN2D", "3. CapsNet"]
+    input_list = ["1. MLP", "2. CNN2D", "3. Sklearn Classifier"]
 
     filereader = csv.reader(open("Data/KDDTrain+.txt"), delimiter=",")
     data = np.array(list(filereader))
@@ -246,45 +211,53 @@ def main():
 
     P1 = input("Choisissez le reseau: " + ' '.join(input_list) + "\n")
     CNN = False
-    if(P1=='2' or P1=='3'): 
+    if(P1 == '2' ):
         CNN = True
 
-    x_train, y_train, x_validation, y_validation, x_test, y_test = preporcess(data, dataTest, CNN)
+    x_train, y_train, x_validation, y_validation, x_test, y_test = preporcess(
+        data, dataTest, CNN)
 
     if (P1 == '1'):
          ### MLP model ###
         model = generate_model(41)
-        model.compile(loss='categorical_crossentropy',
-              optimizer=optimizers.Adam(lr=0.001),metrics=['accuracy'])
-    
+        model.compile(loss='categorical_crossentropy',optimizer=optimizers.Adam(lr=0.001), metrics=['accuracy'])
+
     elif (P1 == '2'):
         ### CNN 2D model ##
         model = generate_cnn_model(41)
 
         # initiate RMSprop optimizer
-        opt =optimizers.Adam(lr=0.0005)
-        model.compile(loss='categorical_crossentropy',optimizer=opt,metrics=['accuracy'])
-    elif (P1 =='3'):
-        # compile the model
-        # define model
-        model = CapsNet(input_shape=x_train.shape[1:], n_class=len(np.unique(np.argmax(y_train, 1))), routings=3)
-        model.compile(optimizer=optimizers.Adam(lr=0.001),
-                  loss=[margin_loss, 'mse'],
-                  loss_weights=[1., 0.392],
-                  metrics={'capsnet': 'accuracy'})
+        opt = optimizers.Adam(lr=0.0005)
+        model.compile(loss='categorical_crossentropy',
+                      optimizer=opt, metrics=['accuracy'])
+    elif (P1 == '3'):
 
-    print(model.summary())
+        model = KerasClassifier(build_fn=create_model, verbose=0)
+
+        # grid search epochs, batch size and optimizer
+        opt = ['rmsprop', 'adam']
+        init = ['glorot_uniform', 'normal', 'uniform']
+        epochs = [5,10,20]
+        batches = [20,50,100]
+    
+        param_grid = dict(optimizer=opt, epochs=epochs,
+                  batch_size=batches, init=init)
+        grid = GridSearchCV(estimator=model, param_grid=param_grid)
+        grid_result = grid.fit(x_train, y_train, validation_data=(x_validation, y_validation))
+
+        # summarize results
+        print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+        means = grid_result.cv_results_['mean_test_score']
+        stds = grid_result.cv_results_['std_test_score']
+        params = grid_result.cv_results_['params']
+        
+        for mean, stdev, param in zip(means, stds, params):
+            print("%f (%f) with: %r" % (mean, stdev, param))
 
     if (P1 != '3'):
-        model.fit(x_train, y_train,validation_data=(x_validation,y_validation), epochs=10, batch_size=50,  callbacks=[TensorBoard(log_dir='/tmp/neuralnet')])
+        model.fit(x_train, y_train,validation_data=(x_validation,y_validation), epochs=1, batch_size=100,  callbacks=[TensorBoard(log_dir='/tmp/neuralnet')])
 
-    else:
-        model.fit([x_train, y_train], [y_train, x_train], batch_size=50, epochs=10, validation_data=[[x_validation, y_validation], [y_validation, x_validation]])
-
-    eval(model, x_test, y_test)
-
-    model.save_weights("./result/trained_model.h5")
-    print("Trained model saved to ./result/trained_model.h5")
+        eval(model, x_test, y_test)
 
 
 if __name__ == "__main__":
